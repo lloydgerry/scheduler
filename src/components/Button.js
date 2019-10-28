db19b38052cb33ea5dc693ccddf8e45162250289
Lloyd <lloyd.gerry@gmail.com>
2019-10-28 12:56:55 -0700

    weekend work completed: button now working

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 import "components/Button.scss";
+const classnames = require('classnames');
 
-export default function Button(props) {
-   return <></>;
-}
+   export default function Button(props) {
+      const buttonClass = classnames("button", {
+         "button--confirm": props.confirm,
+         "button--danger": props.danger
+       });
+    
+      return ( <button 
+      className={buttonClass}
+      onClick={props.onClick}
+      disable={props.disabled}
+      >
+      {props.children}
+      </button>
+      );
+    }
+
--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -10,22 +10,22 @@ it("renders without crashing", () => {
   render(<Button />);
 });
 
-it.skip("renders its `children` prop as text", () => {
+it("renders its `children` prop as text", () => {
   const { getByText } = render(<Button>Default</Button>);
   expect(getByText("Default")).toBeInTheDocument();
 });
 
-it.skip("renders a default button style", () => {
+it("renders a default button style", () => {
   const { getByText } = render(<Button>Default</Button>);
   expect(getByText("Default")).toHaveClass("button");
 });
 
-it.skip("renders a confirm button", () => {
+it("renders a confirm button", () => {
   const { getByText } = render(<Button confirm>Confirm</Button>);
   expect(getByText("Confirm")).toHaveClass("button--confirm");
 });
 
-it.skip("renders a danger button", () => {
+it("renders a danger button", () => {
   const { getByText } = render(<Button danger>Danger</Button>);
   expect(getByText("Danger")).toHaveClass("button--danger");
 });
